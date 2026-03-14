14904712054d5fecbee896c786ed31f1a8deeb5a
asad-ac <asadachau@gmail.com>
2026-03-14 17:13:22 -0400

    pages working with api calls

--- client/src/pages/LocationEvents.jsx
+++ client/src/pages/LocationEvents.jsx
@@ -1,34 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import Event from '../components/Event'
+import LocationsAPI from '../services/LocationsAPI'
 import '../css/LocationEvents.css'
 
-const LocationEvents = ({index}) => {
-    const [location, setLocation] = useState([])
+const LocationEvents = ({ index }) => {
+    const [location, setLocation] = useState({})
     const [events, setEvents] = useState([])
+
+    useEffect(() => {
+        const fetchLocationData = async () => {
+            try {
+                const locationData = await LocationsAPI.getLocationById(index)
+                setLocation(locationData)
+
+                const eventsData = await LocationsAPI.getEventsByLocation(index)
+                setEvents(eventsData)
+            }
+            catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchLocationData()
+    }, [index])
 
     return (
         <div className='location-events'>
             <header>
                 <div className='location-image'>
-                    <img src={location.image} />
+                    <img src={location.image} alt={location.name} />
                 </div>
 
                 <div className='location-info'>
                     <h2>{location.name}</h2>
-                    <p>{location.address}, {location.city}, {location.state} {location.zip}</p>
+                    <p>{location.city}, {location.state}</p>
+                    <p>{location.description}</p>
                 </div>
             </header>
 
             <main>
                 {
-                    events && events.length > 0 ? events.map((event, index) =>
+                    events && events.length > 0 ? events.map((event) =>
                         <Event
                             key={event.id}
                             id={event.id}
-                            title={event.title}
-                            date={event.date}
-                            time={event.time}
-                            image={event.image}
                         />
                     ) : <h2><i className="fa-regular fa-calendar-xmark fa-shake"></i> {'No events scheduled at this location yet!'}</h2>
                 }
--- client/src/services/EventsAPI.jsx
+++ client/src/services/EventsAPI.jsx
@@ -1,11 +1,19 @@
-const API_BASE = "http://localhost:3000/api"
+const API_BASE = 'http://localhost:3000/api'
 
-export const getEvents = async () => {
-  const response = await fetch(`${API_BASE}/events`)
-  return response.json()
+const getAllEvents = async () => {
+    const response = await fetch(`${API_BASE}/events`)
+    const data = await response.json()
+    return data
 }
 
-export const getEventById = async (id) => {
-  const response = await fetch(`${API_BASE}/events/${id}`)
-  return response.json()
+const getEventsById = async (id) => {
+    const response = await fetch(`${API_BASE}/events/${id}`)
+    const data = await response.json()
+    return data
 }
+
+export default {
+    getAllEvents,
+    getEventsById
+}
+
